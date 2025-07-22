fd24eced2c4f5e0636e3767e99d48bc1e7b5321d
muratakdere <muratakdere@posta.mu.edu.tr>
2025-07-22 10:22:00 +0300

    reports deleted

--- dashboard/dashboard2.php
+++ dashboard/dashboard2.php
@@ -6,7 +6,7 @@
     <title>Mutabık - Ön Muhasebe Dashboard</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
-    <link href="dashboard.css" rel="stylesheet"> </head>
+    <link href="./dashboard.css" rel="stylesheet"> </head>
 <body>
     <?php include __DIR__ . '/sidebar.php'; ?>
 
